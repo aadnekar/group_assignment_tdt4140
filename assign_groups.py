7c8eb54d970d506e583f523dee02d4c67f11ac3f
Ådne Karstad <aadne.karstad@gmail.com>
2021-01-07 21:36:31 +0100

    add column with group number and username to be imported in blackboard
    after groups are created you will be able to create a csv from the file
    with group id and username to add them to the groups in blackboard
    
    small refactors to increase modifiability

--- assign_groups.py
+++ assign_groups.py
@@ -15,88 +15,78 @@ parser.add_argument(
     help="Size of groups",
 )
 parser.add_argument(
-    "-O",
+    "--out",
     type=str,
     default="assigned_groups",
-    help="Filename of output, e.g. '-O result', will output a file result.xlsx",
+    help="Filename of output, e.g. '--out result', will output a file result.xlsx",
 )
 parser.add_argument("-f", type=str, help="Path to file to process")
 args = parser.parse_args()
 
 ATTRIBUTES_MAP = {
-    "A": "name",
-    "B": "email",
-    "C": "provided_email",
-    "D": "prog_skill",
-    "E": "taken_it1901",
-    "F": "grant_anonym",
-    "G": "availability",
-    "H": "ref_group",
-    "I": "eng_group",
-    "J": "program",
+    "B": "username",
+    "C": "name",
+    "D": "email",
+    "E": "provided_email",
+    "F": "programming_experience",
+    "G": "completed_it1901",
+    "J": "reference_group",
+    "I": "available_on_compulsory_dates",
+    "H": "granted_permission_for_deliveries",
+    "K": "program",
+}
+
+ATTRIBUTES_IN_MAP = {
+    # "group": -1,
+    # "username": -1,
+    "name": 4,
+    "email": 3,
+    "provided_email": 5,
+    "programming_experience": 7,
+    "completed_it1901": 6,
+    "reference_group": 11,
+    "available_on_compulsory_dates": 8,
+    "granted_permission_for_deliveries": 10,
+    "program": 9,
+}
+
+ATTRIBUTES_TO_PRETTY_FORMAT = {
+    "group": "Gruppe",
+    "username": "Brukernavn",
+    "name": "Navn",
+    "email": "Email",
+    "provided_email": "Gitt email",
+    "programming_experience": "Programmeringsferdighet",
+    "completed_it1901": "Gjennomført IT1901",
+    "reference_group": "Ønsker å være i referansegruppe",
+    "available_on_compulsory_dates": "Tilgjengelig under produktgjennomgang",
+    "granted_permission_for_deliveries": "Gitt tillatelse for bruk av anonymiserte leveranser",
+    "program": "Studieprogram",
 }
 
 
 class Student(object):
     """ Object to represent the required fields of students in TDT4140 """
 
-    def __init__(
-        self,
-        email,
-        provided_email,
-        name,
-        taken_it1901,
-        prog_skill,
-        availability,
-        program,
-        grant_anonym,
-        ref_group,
-        eng_group,
-    ):
+    def __init__(self, **kwargs):
         """
-        email:              provided by logged in user
-        provided_email:     written by user
-        name:               name
-        taken_it1901        student has completed the couse 1901
-        prog_skill          self described skill ranging from 1-5
-        availability:       for given dates that demos are run
-        program:            study program that students are enrolled in
-        grant_anonym:       we are allowed to use their deliveries to train assistants
-        ref_group:          student wants to join the reference group
-        eng_group:          student wants to attend an engelish speaking group
-
-        Student objects should provide attributes to sort on so that we can assign the student to a
-        prefered group.
-
-        Should be sorted on if the student have positive grant_anonym, taken_it1901, prog_skill
+            Values that should be assigned to a student object should be adjusted in the directories
+            at the beginning of the file.
         """
 
-        self.email = email
-        self.provided_email = provided_email
-        self.name = name
-        self.taken_it1901 = taken_it1901
-        self.prog_skill = prog_skill
-        self.availability = availability
-        self.program = program
-        self.grant_anonym = grant_anonym
-        self.ref_group = ref_group
-        self.eng_group = eng_group
+        self.__dict__.update(kwargs)
+        if "email" in kwargs.keys():
+            self.username = self.get_username(kwargs["email"])
 
-    def get_prog_skill(self):
-        return self.prog_skill
+    @staticmethod
+    def get_username(email):
+        return email.split("@")[0] if email else ""
+
+    def get_programming_experience(self):
+        return self.programming_experience if self.programming_experience else 0
 
     def __getattribute__(self, name: str):
         return super().__getattribute__(name)
-
-    def __repr__(self) -> str:
-        return f"{self.provided_email} | skill={self.prog_skill} | grant_anonym={self.grant_anonym}"
-
-
-def write_column_names(sheet):
-    """ Add column name to row 1 """
-    for key, attribute in zip(ATTRIBUTES_MAP.keys(), ATTRIBUTES_MAP.values()):
-        sheet[f"{key}1"] = f"{attribute}"
-
 
 def store_students_in_list(workbook, students=None):
     """ Temporal storage for students to be sorted """
@@ -104,22 +94,15 @@ def store_students_in_list(workbook, students=None):
         students = []
 
     for i, student in enumerate(workbook.active.values):
+        # Do not include the name of columns row
         if i == 0:
             continue
-        students.append(
-            Student(
-                email=student[3],
-                name=student[4],
-                provided_email=student[5],
-                taken_it1901=student[6],
-                prog_skill=student[7],
-                availability=student[8],
-                program=student[9],
-                grant_anonym=student[10],
-                ref_group=student[11],
-                eng_group=student[12],
-            )
-        )
+
+        student_attr = {}
+        for key, value in zip(ATTRIBUTES_IN_MAP.keys(), ATTRIBUTES_IN_MAP.values()):
+            student_attr[key] = student[value]
+
+        students.append(Student(**student_attr))
 
     return students
 
@@ -144,22 +127,27 @@ def sort_students(students):
     and least significant of programming skill.
     """
 
-    students = sorted(students, key=lambda stud: stud.prog_skill)
     students = sorted(
-        students,
-        key=lambda stud: convert_answer_to_bool(stud.taken_it1901),
-        reverse=True,
+        students, key=lambda stud: getattr(stud, "programming_experience")
     )
     students = sorted(
         students,
-        key=lambda stud: convert_answer_to_bool(stud.grant_anonym),
+        key=lambda stud: convert_answer_to_bool(getattr(stud, "completed_it1901")),
         reverse=True,
     )
 
     return students
 
 
-def write_student_to_sheet(sheet, row, student=None):
+def write_column_names(sheet):
+    """ Add column name to row 1 """
+    for key, attribute in zip(ATTRIBUTES_MAP.keys(), ATTRIBUTES_MAP.values()):
+        sheet[f"{key}1"] = f"{ATTRIBUTES_TO_PRETTY_FORMAT[attribute]}"
+
+
+def write_student_to_sheet(sheet, row, group_index, student=None):
+    """ Write a single student to the active worksheet """
+    sheet[f"A{row}"] = f"Group {group_index + 1}"
     for key, attribute in zip(ATTRIBUTES_MAP.keys(), ATTRIBUTES_MAP.values()):
         sheet[f"{key}{row}"] = f"{getattr(student, attribute)}"
 
@@ -179,23 +167,13 @@ def assign_students_to_groups(
         write_student_to_sheet(
             sheet,
             row=calculate_row(group_index, group_size, offset_value, student_index),
+            group_index=group_index,
             student=students.pop(),
         )
 
         group_index += 1
 
     return sheet
-
-
-def write_group_number_headers(number_of_groups, sheet, group_size=8, offset_value=2):
-    """ Adds the group numbers to where rows students should be appended to """
-    for group_number in range(number_of_groups):
-        sheet[
-            f"A{calculate_row(group_number, group_size=group_size, offset_value=offset_value)}"
-        ] = f"Group: {group_number + 1}"
-
-    return sheet
-
 
 def calculate_row(group_index, group_size, offset_value=2, student_index=0):
     """
@@ -228,12 +206,12 @@ def main():
     sheet = new_workbook.active
 
     write_column_names(sheet)
-    write_group_number_headers(sheet=sheet, number_of_groups=number_of_groups)
+    # write_group_number_headers(sheet=sheet, number_of_groups=number_of_groups)
     assign_students_to_groups(
-        sheet=sheet, number_of_groups=number_of_groups, students=students
+        sheet=sheet, number_of_groups=number_of_groups, group_size=group_size, students=students
     )
 
-    new_workbook.save(filename=f"{args.O}.xlsx")
+    new_workbook.save(filename=f"{args.out}.xlsx")
 
 
 if __name__ == "__main__":
